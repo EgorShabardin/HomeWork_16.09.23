31ae15f220741feb52d0417e109a0f45a9b42a74
EgorShabardin <eg.shabardin@gmail.com>
2023-09-18 18:58:58 +0300

    Добавьте файлы проекта.

---
+++ Тумаков - Лабораторная работа №2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Тумаков___Лабораторная_работа__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Упражнение 2.1. Программа приветствует пользователя по имени.
+            Console.WriteLine("{0, 88}", "УПРАЖНЕНИЕ 2.1. ПРОГРАММА ПРИВЕТСТВУЕТ ПОЛЬЗОВАТЕЛЯ ПО ИМЕНИ \n");
+
+            string userName;
+
+            Console.Write("Введите ваше имя: ");
+            userName = Console.ReadLine();
+
+            Console.WriteLine($"Здравствуйте, {userName}");
+
+
+            // Упражнение 2.2. Программа получает два числа и делит их.
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("{0, 85}", "УПРАЖНЕНИЕ 2.2. ПРОГРАММА ПОЛУЧАЕТ ДВА ЧИСЛА И ДЕЛИТ ИХ \n");
+
+            int firstNumber, secondNumber;
+            double result;
+
+            Console.Write("Введите первое целое число (делимое): ");
+            firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе целое число (делитель): ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+            if (secondNumber == 0)
+            {
+                Console.WriteLine("Деление невозможно. На ноль делить нельзя!");
+            }
+            else
+            {
+                result = (double)firstNumber / secondNumber;
+                Console.WriteLine($"Результатом деления {firstNumber} на {secondNumber} будет {result}");
+            }
+
+
+            // Домашнее задание 2.1. Программа получает букву и выводит следующую за ней букву в алфавитном порядке.
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("{0, 110}", "ДОМАШНЕЕ ЗАДАНИЕ 2.1. ПРОГРАММА ПОЛУЧАЕТ БУКВУ И ВЫВОДИТ СЛЕДУЮЩУЮ ЗА НЕЙ БУКВУ В АЛФАВИТНОМ ПОРЯДКЕ \n");
+
+            char[] Rus = { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я' };
+            char letter;
+            int index;
+
+            Console.Write("Введите букву алфавита русского языка: ");
+            letter = char.Parse(Console.ReadLine().ToUpper());
+
+            index = Array.IndexOf(Rus, letter) + 1;
+
+            if (index == Rus.Length)
+            {
+                Console.WriteLine($"Вы введи букву {letter}. Это последняя буква. Следующей нет!");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели букву {0}. Следующая за ней буква {1}", letter, Rus[index]);
+            }
+
+
+            // Домашнее задание 2.2. Программа находит корни квадратного уравнения.
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("{0, 91}", "ДОМАШНЕЕ ЗАДАНИЕ 2.2. ПРОГРАММА НАХОДИТ КОРНИ КВАДРАТНОГО УРАВНЕНИЯ \n");
+
+            double coefficientA, coefficientB, coefficientC, discriminant, x1, x2, x;
+
+            Console.Write("Введите коэффициент A: ");
+            coefficientA = double.Parse(Console.ReadLine());
+            Console.Write("Введите коэффициент B: ");
+            coefficientB = double.Parse(Console.ReadLine());
+            Console.Write("Введите коэффициент С: ");
+            coefficientC = double.Parse(Console.ReadLine());
+
+            discriminant = (coefficientB * coefficientB) - (4 * coefficientA * coefficientC);
+
+            if (discriminant > 0)
+            {
+                x1 = (-coefficientB + Math.Sqrt(discriminant)) / (2 * coefficientA);
+                x2 = (-coefficientB - Math.Sqrt(discriminant)) / (2 * coefficientA);
+                Console.WriteLine("Первый корень: " + x1);
+                Console.WriteLine("Второй корень: " + x2);
+            }
+            else if (discriminant == 0)
+            {
+                x = (-coefficientB) / (2 * coefficientA);
+                Console.WriteLine("Единственный корень: " + x);
+            }
+            else
+            {
+                Console.WriteLine("Дискриминант меньше нуля. Корней нет.");
+            }
+        }
+    }
+}
---
+++ Тумаков - Лабораторная работа №3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Тумаков___Лабораторная_работа__3
+{
+    class Program
+    {
+        enum Bank
+        {
+            Текущий_счет,
+            Сберегательный_счет
+        }
+        struct BankAccaunt
+        {
+            public int accauntNumber;
+            public string accauntType;
+            public double accauntBalance;
+            public void dataPrint()
+            {
+                Console.WriteLine($"{accauntType} счет под номером {accauntNumber} содержит {accauntBalance} рублей");
+            }
+        }
+        enum ВУЗ
+        {
+            КГУ,
+            КАИ,
+            КХТИ
+        }
+        struct Worker
+        {
+            public string personName;
+            public ВУЗ workingPlace;
+            public void dataPrint()
+            {
+                Console.WriteLine($"{personName} работает в {workingPlace}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Упражнение 3.1. Создание перечисляемого типа данных, отображающего виды банковских счетов.
+            Console.WriteLine("{0, 106}", "УПРАЖНЕНИЕ 3.1. СОЗДАНИЕ ПЕРЕЧИСЛЯЕМОГО ТИПА ДАННЫХ, ОТОБРАЖАЮЩЕГО ВИДЫ БАНКОВСКИХ СЧЕТОВ \n");
+
+            Bank curentAccount = Bank.Текущий_счет;
+            Bank savingAccaunt = Bank.Сберегательный_счет;
+
+            Console.WriteLine($"Банк имеет {curentAccount} и {savingAccaunt}");
+
+
+            // Упражнение 3.2. Создание структуры, которая содержит основную информацию о банкоском счете.
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("{0,107}", "УПРАЖНЕНИЕ 3.2. СОЗДАНИЕ СТРУКТУРЫ, КОТОРАЯ СОДЕРЖИТ ОСНОВНУЮ ИНФОРМАЦИЮ О БАНКОВСКОМ СЧЕТЕ \n");
+
+            BankAccaunt myAccaunt = new BankAccaunt();
+            myAccaunt.accauntType = "Сберегательный";
+            myAccaunt.accauntNumber = 848646268;
+            myAccaunt.accauntBalance = 13483294;
+
+            myAccaunt.dataPrint();
+
+            // Домашнее задание 3.1. Создание перечисляемого типа, содержащего вузы, и структуры с данными работника.
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("{0,110}", "ДОМАШНЕЕ ЗАДАНИЕ 3.1. СОЗДАНИЕ ПЕРЕЧИСЛЯЕМОГО ТИПА, СОДЕРЖАЩЕГО ВУЗЫ, И СТРУКТУРЫ С ДАННЫМИ РАБОТНИКА \n");
+
+            Worker universityWorker = new Worker();
+            universityWorker.personName = "Иванов Иван Иванович";
+            ВУЗ universitiy = ВУЗ.КАИ;
+            universityWorker.workingPlace = universitiy;
+
+            universityWorker.dataPrint();
+        }
+    }
+}
